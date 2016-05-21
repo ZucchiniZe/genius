ccdf39776c2da1c5ec8902f7ad81e95ef34aeda9
Alex B <me@alexb.io>
2016-05-20 20:57:59 -0700

    add markdown support

--- genius/settings.py
+++ genius/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'hijack_admin',
     'compat',
     'registration',
+    'django_markwhat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
