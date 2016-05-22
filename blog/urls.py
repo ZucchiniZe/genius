4a726f38d2c85284857e1d8b39aa1b4661044cfb
Alex B <me@alexb.io>
2016-05-22 12:57:09 -0700

    remove one last url

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^create/$', views.edit, name='create'),
     url(r'^(?P<pk>[0-9]+)/$', views.detail, name='detail'),
 ]
