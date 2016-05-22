48f7e4e494c49cab24dbc6519965dc113eed4c06
Alex B <me@alexb.io>
2016-05-22 12:55:47 -0700

    fix urls

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^create/$', views.edit, name='create'),
     url(r'^(?P<pk>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<pk>[0-9]+)/edit$', views.edit, name='edit'),
-    url(r'^(?P<pk>[0-9]+)/delete/$', views.delete, name='delete'),
 ]
