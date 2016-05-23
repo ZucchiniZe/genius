4f96f3dfcd9f3f7fd6047ecd6d5e6aa61ca9535a
Alex B <me@alexb.io>
2016-05-22 19:46:45 -0700

    add logo, index reason, and non-auth page

--- peak/views.py
+++ peak/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponseForbidden
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
@@ -10,29 +10,41 @@ from .forms import PeakForm
 
 def index(request):
     peaks = Peak.objects.filter(private=False).order_by('-pub_date')[:20]
-    return render(request, 'peak/index.html', {'peaks': peaks})
+    return render(request, 'peak/index.html', {
+        'peaks': peaks,
+        'reason': 'Viewing the 20 most recent peaks'
+    })
 
 
 def index_tag(request, tag):
     peaks = Peak.objects.filter(private=False, tags__name__in=[tag]).distinct()
-    return render(request, 'peak/index.html', {'peaks': peaks})
+    return render(request, 'peak/index.html', {
+        'peaks': peaks,
+        'reason': 'Viewing all the peaks tagged {}'.format(tag)
+    })
 
 
 def index_user(request, username):
     peaks = Peak.objects.filter(private=False, user__username__iexact=username)
-    return render(request, 'peak/index.html', {'peaks': peaks})
+    return render(request, 'peak/index.html', {
+        'peaks': peaks,
+        'reason': "Viewing all of {}'s peaks".format(username)
+    })
 
 
 @login_required
 def personal_index(request):
     peaks = Peak.objects.filter(user=request.user, private=True)
-    return render(request, 'peak/index.html', {'peaks': peaks})
+    return render(request, 'peak/index.html', {
+        'peaks': peaks,
+        'reason': 'Viewing all of your private peaks'
+    })
 
 
 def detail(request, pk):
     peak = get_object_or_404(Peak, pk=pk)
     if peak.private and (request.user and peak.user != request.user):
-        return HttpResponseForbidden()
+        return render(request, 'peak/not-auth.html', status=403)
     return render(request, 'peak/detail.html', {'peak': peak})
 
 
@@ -41,7 +53,7 @@ def edit(request, pk=None):
     if pk:
         peak = get_object_or_404(Peak, pk=pk)
         if peak.user != request.user:
-            return HttpResponseForbidden()
+            return render(request, 'peak/not-auth.html', status=403)
     else:
         peak = Peak(user=request.user)
 
@@ -61,4 +73,4 @@ def delete(request, pk):
         peak.delete()
         return HttpResponseRedirect(reverse('peak:index'))
     else:
-        return HttpResponseForbidden()
+        return render(request, 'peak/not-auth.html', status=403)
